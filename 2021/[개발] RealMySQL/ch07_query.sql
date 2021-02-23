32a0f6eaf1fe0010c6f7d1922fde030ab52f0d4e
YoungChulShin <go1323@gmail.com>
2021-02-23 10:03:12 +0900

    7장 쿼리 추가중

--- 2021/[개발] RealMySQL/ch07_query.sql
+++ 2021/[개발] RealMySQL/ch07_query.sql
@@ -40,5 +40,27 @@ select concat_ws(',', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 ## Group by 문자열 결합
 show variables where Variable_name = 'group_concat_max_len';
 
+## 값 비교와 대체
+SET @gender = 'M';
+select @gender,
+       case @gender when 'M' then 'Man'
+                    when 'W' then 'Woman'
+                    else 'Unknown'
+       end as longGender;
+
+select now(),
+       case when now() < '2021-01-01 00:00:00' then 'old'
+            else 'new'
+       end;
+
+## 타입의 전환(CAST, CONVERT)
+select cast('1234' as signed integer) as converted_integer, cast('2000-01-01' as date) as converted_date,
+       1 - 2, cast(1 - 2 as unsigned );
+
+## 암호화 및 해시 함수(MD5, SHA)
+select MD5('abc'), sha('abc'), sha1('abc'), sha2('abc', 224);
+
+## 처리 대기(sleep)
+select sleep(10);
 
 
