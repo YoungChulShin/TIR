6e9e4127c42a8a4b704bf05c67f34b511c4cd63e
YoungChulShin <go1323@gmail.com>
2021-03-02 18:51:55 +0900

    query 업데이트

--- 2021/[개발] RealMySQL/ch07_query.sql
+++ 2021/[개발] RealMySQL/ch07_query.sql
@@ -107,10 +107,17 @@ EXPLAIN SELECT * FROM employees WHERE first_name = 10001;
 ## 날짜 비교
 SELECT DATE(NOW()), NOW();
 
+EXPLAIN SELECT COUNT(*) FROM employees WHERE hire_date < '2011-07-23 11:10:12';
+EXPLAIN SELECT COUNT(*) FROM employees WHERE hire_date < UNIX_TIMESTAMP();
+EXPLAIN SELECT COUNT(*) FROM employees WHERE hire_date < FROM_UNIXTIME(UNIX_TIMESTAMP());
+
 ## Distinct
 ### 개별 선택
 EXPLAIN SELECT DISTINCT emp_no FROM salaries;
 EXPLAIN SELECT emp_no FROM salaries GROUP BY emp_no;
+
+SELECT DISTINCT(first_name), last_name FROM employees;
+SELECT DISTINCT first_name, last_name FROM employees;
 
 ### 집합 선택
 EXPLAIN SELECT COUNT(DISTINCT s.salary) FROM employees e, salaries s WHERE e.emp_no = s.emp_no AND e.emp_no BETWEEN 100001 AND 100100;
@@ -119,7 +126,9 @@ SELECT COUNT(DISTINCT first_name), COUNT(DISTINCT last_name) FROM employees WHER
 SELECT COUNT(DISTINCT first_name, last_name) FROM employees WHERE emp_no BETWEEN 10001 AND 10200;
 
 ## LIMIT
+EXPLAIN SELECT * FROM employees WHERE emp_no BETWEEN 10001 AND 10010 ORDER BY first_name LIMIT 0, 10;
 EXPLAIN SELECT * FROM employees LIMIT 0, 10;
+SELECT DISTINCT first_name FROM employees LIMIT 0, 10;
 EXPLAIN SELECT first_name FROM employees GROUP BY first_name LIMIT 0, 10;
 
 ## JOIN
@@ -127,6 +136,7 @@ EXPLAIN SELECT * FROM employees e, dept_emp de WHERE e.emp_no = de.emp_no;
 
 EXPLAIN SELECT * FROM employees e LEFT JOIN dept_manager mgr ON e.emp_no = mgr.emp_no WHERE mgr.dept_no = 'd001';
 EXPLAIN SELECT * FROM employees e LEFT JOIN dept_manager mgr ON e.emp_no = mgr.emp_no AND mgr.dept_no = 'd001';
+
 
 ### ANTI JOIN
 CREATE TABLE tab_test1(id INT, PRIMARY KEY(id));
@@ -152,10 +162,13 @@ explain select * from dept_emp de, employees e where de.dept_no = 'd001' and e.e
 explain select * from (select * from dept_emp where dept_no = 'd001' limit 100, 10) de, employees e where e.emp_no = de.emp_no;
 
 ## Group by
+select first_name, last_name, count(*) from employees group by first_name order by last_name
+
 select  first_name from employees group by gender;
 select first_name, last_name, count(*) from employees group by first_name, last_name order by last_name
 
 ### Group by Order by null
+create index employees_idx_from_date on salaries (from_date);
 explain select from_date from salaries group by from_date;
 explain select from_date from salaries group by from_date order by null;
 
@@ -164,7 +177,11 @@ select dept_no, count(*) from dept_emp group by dept_no with rollup ;
 select first_name, last_name, count(*) from employees group by first_name, last_name with rollup ;
 
 ## Order by
+select first_name, last_name from employees order by 'ycshin';
 explain select first_name, last_name from employees order by rand();
+
+explain SELECT * FROM employees ORDER BY emp_no;
+explain SELECT * FROM employees ORDER BY emp_no+10;
 
 ## sub query
 ### 상관 서브쿼리
@@ -175,7 +192,12 @@ where exists(
     select 1
     from dept_emp de
     where de.emp_no = e.emp_no
-      and de.from_date between '2000-01-01' and '2011-12-30')
+      and de.from_date between '2000-01-01' and '2011-12-30');
+
+
+UPDATE departments
+SET dept_name = (SELECT CONCAT(dept_name, '2') FROM departments WHERE dept_no='d009')
+WHERE dept_no='d001';
 
 ### select sub query
 select emp_no, (select dept_name from departments where dept_name = 'Sales1')
@@ -201,6 +223,15 @@ select sql_no_cache
 from employees e1, employees e2
 where e1.emp_no = e2.emp_no;
 
+create index employees_idx_first_name on employees (first_name);
+explain
+select * from dept_emp de
+where de.emp_no =
+    (select emp_no
+     from   employees
+     where first_name = 'Georgi'
+       and last_name = 'facello' limit 1);
+
 ### where in subquery
 explain
 select * from dept_emp de
@@ -208,8 +239,13 @@ where de.dept_no in (
     select dept_no from departments where dept_name = 'Finance');
 
 ### from subquery
+Created_tmp_disk_tables,0
+Created_tmp_files,32
+Created_tmp_tables,6
+
+show status like 'Created_tmp%';
 explain
-select * from (
+select sql_no_cache * from (
   select *
   from (
    select *
@@ -218,10 +254,12 @@ select * from (
 ) y;
 
 ## Union
+explain
 selecT sql_no_cache * from employees where emp_no between 10001 and 200000
 union distinct
 selecT sql_no_cache * from employees where emp_no between 200001 and 500000;
 
+explain
 selecT sql_no_cache * from employees where emp_no between 10001 and 200000
 union all
 selecT sql_no_cache * from employees where emp_no between 200001 and 500000;
